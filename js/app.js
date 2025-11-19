fa1a9cd3ada90b8b447f3ea32e0453391f3ba056
willi <ninasweet480@gmail.com>
2025-11-18 19:01:32 -0800

    saving after second question

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
-//Exercise 1 has been completed for you: Using as example
+// //Exercise 1 has been completed for you: Using as example
 
-const maxOfTwoNumbers = (x, y) => {
-    if (x >= y) {
-        return x;
-    } else {
-        return y;
+// const maxOfTwoNumbers = (x, y) => {
+//     if (x >= y) {
+//         return x;
+//     } else {
+//         return y;
+//     }
+// }
+//Excercise 2
+// console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+function isAdult(age){
+
+    if (age <= 18){
+        return "Minor";}
+    else
+        {return "Adult";
     }
 }
-
-console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+console.log('Exercise 2 Result:', isAdult(21));
