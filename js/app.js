e3df1f6bf4e21ab56e7767ac68f515452dfc5ad3
willi <ninasweet480@gmail.com>
2025-11-18 18:31:29 -0800

    Saving after first question

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//Exercise 1 has been completed for you: Using as example
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
