b8e375b4bb009b6006d8feefa3bddfc38331ee58
willi <ninasweet480@gmail.com>
2025-11-18 20:02:43 -0800

    test

--- js/app.js
+++ js/app.js
@@ -11,8 +11,7 @@
 // console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 //Excercise 2
 function isAdult(age){
-
-    if (age <= 18){
+     if (age <= 18){
         return "Minor";}
     else
         {return "Adult";
@@ -21,4 +20,17 @@ function isAdult(age){
 console.log('Exercise 2 Result:', isAdult(21));
 
 //Excercise 3
+
+function isCharAVowel(vowel){
+    if (vowel === "a" || 
+        vowel === "e" ||
+        vowel === "i" || 
+        vowel === "o" ||
+        vowel === "u")
+        {return "true"}
+    else
+        {return "false"
+    }
+}
+console.log('Exercise 3 Result:', isCharAVowel("a"));
 
