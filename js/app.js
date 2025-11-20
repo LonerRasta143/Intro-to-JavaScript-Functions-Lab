22f2f02211ea61464885ac78bfd2dfdfebf087d5
willi <ninasweet480@gmail.com>
2025-11-19 18:24:04 -0800

    Commit after 6

--- js/app.js
+++ js/app.js
@@ -52,8 +52,16 @@ function greetUser(name, timeOfDay){
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 
 //excercise 6
-function maxOfThree(a, b, c){
-    
-
+function maxOfThree(a, b, c) {
+    if (a>b || a>c) {
+        return a
+    }
+    else if (b>a ||b>c){
+        return b
+    }
+    else if (c>a || c>b){
+        return c
+    }
 
 }
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
